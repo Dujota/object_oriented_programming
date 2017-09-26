ced48a56579138966caf97543b3c73657a4eb1f0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-25 23:57:27 -0400

    added two instances of cat and created the initialize method

---
+++ cat.rb
@@ -0,0 +1,18 @@
+class Cat
+
+  def initialize
+    @name = ''
+    @preferred_food = ''
+    @meal_time = 0
+  end
+
+
+
+end
+
+cat1 = Cat.new
+cat2 = Cat.new
+
+puts cat1.inspect
+puts '-'*33
+puts cat2.inspect
